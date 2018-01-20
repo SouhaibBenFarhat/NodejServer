7e9b46b562c3227ae14cfa7590a037474b4ba1e6
souhaibbenfarhat <souhaib.benfarhat@esprit.tn>
2018-01-21 00:48:17 +0100

    business type

--- models/businessType.model.js
+++ models/businessType.model.js
@@ -5,6 +5,14 @@ const businessTypeSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
+    },
+    logo: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        default: ""
     }
 });
 
